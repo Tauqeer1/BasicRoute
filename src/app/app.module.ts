976ec8857c679142c64b6b5129f46c724633ce6b
tauqeer <tauqeer.shakir@yahoo.com>
2016-11-26 18:02:21 +0500

    Basic Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { RouterApp } from './app.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    RouterApp,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    //Default location strategy is PathLocationStrategy , here we are using HashLocationStrategy for routing
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    // { provide: APP_BASE_HREF, useValue: '/'}
+  ],
+  bootstrap: [RouterApp]
 })
 export class AppModule { }
